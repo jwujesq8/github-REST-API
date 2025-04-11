b50133895857bbc6a5ec86f8f24ec5bc83798679
jwujesq8 <zhukovskaja.elena@gmail.com>
2025-04-11 15:19:43 +0200

    moved token to the file

--- src/main/java/com/api/config/WebClientConfig.java
+++ src/main/java/com/api/config/WebClientConfig.java
@@ -6,14 +6,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebClientConfig {
 
-    @Value("${github.api.token}")
     private String GITHUB_API_TOKEN;
 
     @Bean
-    public WebClient webClient(){
+    public WebClient webClient(@Value("${github.api.token.path}") String githubApiTokenPath)
+            throws IOException {
+        Path a = Paths.get(githubApiTokenPath).toAbsolutePath().normalize();
+        if (Files.exists(a)) {
+            GITHUB_API_TOKEN = new String(Files.readAllBytes(a));
+        } else {
+            throw new IOException("githubToken file not found: " + githubApiTokenPath);
+        }
+
         return WebClient.builder()
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + GITHUB_API_TOKEN)
                 .baseUrl("https://api.github.com")
